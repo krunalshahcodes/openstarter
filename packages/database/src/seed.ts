911cd4f377ab1bdcec2aa502a61a9fd951adefcf
Krunal Shah <shahkrunal7091@gmail.com>
2024-04-08 22:02:35 +0530

    fix: database seeder

--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -10,6 +10,7 @@ const main = async () => {
       email: "john@openstarter.dev",
       name: "John Doe",
       password: "$2a$12$oFPjsI3zfjN7v.V6UhpY4u/2z91Hkl9C12Ci8y9qmXMFZLRj6cAsa", // Password123#
+      emailVerified: new Date(Date.now()),
     },
   });
 
